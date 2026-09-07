eb893edc1c438d84a4717daab5ae5830915591b7
ritwik17c <69358070+ritwik17c@users.noreply.github.com>
2026-09-08 03:20:55 +0530

    Load selectable theme controller on VKVTT Home

--- vkv-home-notice-attachments.js
+++ vkv-home-notice-attachments.js
@@ -1,3 +1,4 @@
+import'./vkv-theme.js?v=20260908-theme1';
 import'./vkv-cache-bootstrap.js?v=20260908-production-cache-1';
 import{getApps,getApp}from'https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js';
 import{getAuth,onAuthStateChanged}from'https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js';
